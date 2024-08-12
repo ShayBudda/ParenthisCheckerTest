9c6daa11af3696fae3a54b3dda077f340a68ac8d
ShayBudda <169202119+ShayBudda@users.noreply.github.com>
2024-08-12 09:15:29 -0700

    Add files via upload

---
+++ ParenthesisCheckerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParenthisChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParenthisChecker.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ParenthesisCheckerTests
+{
+    [TestMethod]
+    public void TestSingleTypeBrackets()
+    {
+        Assert.IsTrue(ParenthesisChecker.IsBalanced("()"));
+        Assert.IsFalse(ParenthesisChecker.IsBalanced("("));
+    }
+
+    [TestMethod]
+    public void TestMixedTypeBrackets()
+    {
+        Assert.IsTrue(ParenthesisChecker.IsBalanced("{[()]}"));
+        Assert.IsFalse(ParenthesisChecker.IsBalanced("{[(])}"));
+    }
+
+    [TestMethod]
+    public void TestBalancedScenarios()
+    {
+        Assert.IsTrue(ParenthesisChecker.IsBalanced("a(b)c[d]{e}"));
+        Assert.IsTrue(ParenthesisChecker.IsBalanced("a + b - (c * d / {e + f})"));
+    }
+
+    [TestMethod]
+    public void TestUnbalancedScenarios()
+    {
+        Assert.IsFalse(ParenthesisChecker.IsBalanced("a(b]c[d}e"));
+        Assert.IsFalse(ParenthesisChecker.IsBalanced("({[)]}"));
+    }
+
+    [TestMethod]
+    public void TestEmptyString()
+    {
+        Assert.IsTrue(ParenthesisChecker.IsBalanced(""));
+    }
+}
